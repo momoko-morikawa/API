59c6e293a01685b3c93770702b271c5ef437379e
momoko.morikawa <morikawa.momoko@itfllc.co.jp>
2024-04-19 16:05:02 +0900

    全件取得機能

--- src/main/java/com/example/springTodo/controller/TodoListController.java
+++ src/main/java/com/example/springTodo/controller/TodoListController.java
@@ -1,8 +1,12 @@
 package com.example.springTodo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springTodo.model.TodoList;
 import com.example.springTodo.service.TodoService;
 
 
@@ -13,6 +17,16 @@ public class TodoListController {
 	@Autowired
 	private TodoService todoService;
 	
-	//
+	//TODOの追加
+	@PostMapping("/todos")
+	public void addTodo(@RequestBody TodoList todoList) {
+		todoService.addTodo(todoList);
+	}
+	
+	//全件取得
+	@GetMapping("/todos")
+	public void getAll(){
+		todoService.getAll();
+	}
 
 }
