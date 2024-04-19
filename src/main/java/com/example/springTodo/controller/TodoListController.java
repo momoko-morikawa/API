1ef424a75a7a4488e4aa8994ef5ce7a870b2643a
momoko.morikawa <morikawa.momoko@itfllc.co.jp>
2024-04-19 16:09:56 +0900

    基本機能完成

--- src/main/java/com/example/springTodo/controller/TodoListController.java
+++ src/main/java/com/example/springTodo/controller/TodoListController.java
@@ -1,7 +1,9 @@
 package com.example.springTodo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,17 @@ public class TodoListController {
 	@GetMapping("/todos")
 	public void getAll(){
 		todoService.getAll();
+	}
+	
+	//一件取得
+	@GetMapping("/todos/{id}")
+	public void getTodo(@PathVariable("id") int id) {
+		todoService.getTodo(id);
+	}
+	
+	@DeleteMapping("/todos/{id}")
+	public void deleteTodo(@PathVariable("id") int id) {
+		todoService.deleteTodo(id);
 	}
 
 }
