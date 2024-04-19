191e1361b5af8f3f8230a61a3d612e6150cf9766
momoko.morikawa <morikawa.momoko@itfllc.co.jp>
2024-04-19 15:05:45 +0900

    全部くっつけた

--- src/main/java/com/example/springTodo/service/TodoService.java
+++ src/main/java/com/example/springTodo/service/TodoService.java
@@ -6,14 +6,16 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.example.springTodo.model.TodoList;
+
 @Service
-public class ItemService {
+public class TodoService {
 	
 	//TodoListが沢山入ったリストを作成、３つやることを入れる
 	private List<TodoList> allTodo = new ArrayList<>(Arrays.asList(
-			new TodoList("1","メールを送る","未着手","先生にメールする"), //TodoListコンストラクタ
-			new TodoList("2","お肉を買う","未着手","スーパーで買う"), //TodoListコンストラクタ
-			new TodoList("3","宿題をする","進行中","算数プリント") //TodoListコンストラクタ
+			new TodoList(1,"メールを送る","未着手","先生にメールする"), //TodoListコンストラクタ
+			new TodoList(2,"お肉を買う","未着手","スーパーで買う"), //TodoListコンストラクタ
+			new TodoList(3,"宿題をする","進行中","算数プリント") //TodoListコンストラクタ
 			));
 	
 	//TODOの追加
